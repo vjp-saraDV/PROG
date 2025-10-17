a401f52175a7475bffdcdeee0184f5894c25e8d6
vjp-saraDV <sdelgadov02@informatica.iesvalledeljerteplasencia.es>
2025-10-17 11:04:04 +0200

    ejercicio 27 hecho

--- Tema3/Ejercicio27/src/ejercicio27/Ejercicio27.java
+++ Tema3/Ejercicio27/src/ejercicio27/Ejercicio27.java
@@ -29,7 +29,11 @@ public class Ejercicio27 {
         *Nota 2: Controla el caso de división entre 0 mediante la captura
         *de excepciones.*/
         int a,b,menu,c;
+        /*Declaro las variables a, b, c y menu.*/
+        
+        
         Scanner entrada=new Scanner(System.in);
+        
         System.out.println("Por favor, indique un número.");
         a=entrada.nextInt();
         
@@ -37,16 +41,61 @@ public class Ejercicio27 {
         b=entrada.nextInt();
         
         
-        System.out.println("Introduzca 1 para sumar los dos números introducidos.");
-        System.out.println("Introduzca 2 para restar los dos números introducidos.");
-        System.out.println("Introduzca 3 para multiplicar los dos números introducidos.");
-        System.out.println("Introduzca 4 para dividir los dos números introducidos.");
-        System.out.println("Introduzca 5 para salir del programa.");
-        try{
-            menu=entrada.nextInt;
+        do{
+            System.out.println("Introduzca 1 para sumar los dos números introducidos.");
+            System.out.println("Introduzca 2 para restar los dos números introducidos.");
+            System.out.println("Introduzca 3 para multiplicar los dos números introducidos.");
+            System.out.println("Introduzca 4 para dividir los dos números introducidos.");
+            System.out.println("Introduzca 5 para salir del programa.");
+            menu=entrada.nextInt();
+            
+            switch (menu) {//Debe ser short, int, byte o char.
+                case 1: {
+                    c = a + b;
+                    System.out.println("El resultado de la suma de " + a + " y "
+                            + b + " es: " + c);
+                    break;
+                }
+                case 2: {
+                    c = a - b;
+                    System.out.println("El resultado de la resta de " + a + " y "
+                            + b + " es: " + c);
+                    break;
+                }
+                case 3: {
+                    c = a * b;
+                    System.out.println("El resultado de la multiplicación de "
+                            + a + " y " + b + " es: " + c);
+                    break;
+                }
+                case 4: {
+                    if (b > 0) {
+                        c = a / b;
+                        System.out.println("El resultado de la división de " + a
+                                + " y " + b + " es: " + c);
+                    } else if (b <= 0) {
+                        try {
+                            c = a / b;
+                        }
+                        catch (ArithmeticException e) {
+                            System.out.println("Error: " + e.getMessage());
+                            c = 0;//Asigno el valor 0 para que continue el programa.
+                        }
+                    }
+                    break;
+                }
+                case 5: {
+                    System.exit(0);
+                    break;
+                }
+                default: {
+                    System.out.println("El número introducido no es válido.");
+                    break;
+                }
+            }
         }
-        menu=entrada.nextInt;
-        if
+        
+        while (menu!=5);
     }
     
 }
